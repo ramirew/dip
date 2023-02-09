98e65b010cfad41446676ce1308cec0f22b5be8d
Bryan Gualoto <CLUBRELAMPAGOJR13@GMAIL.COM>
2023-02-09 14:54:28 -0500

    G
    G

--- feature_ext_sel/shape/geometry/libreriaGeometria.cpp
+++ feature_ext_sel/shape/geometry/libreriaGeometria.cpp
@@ -1,33 +1,12 @@
 #include "libreriaGeometria.h"
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <cmath>
 
-using namespace std;
+// Constructor
+ConvexHull::ConvexHull() {}
 
-// Estructura para almacenar un punto en 2D
-struct Point {
-    int x, y;
-    Point(int x = 0, int y = 0) : x(x), y(y) {}
-};
-
-// Función para comparar dos puntos por su ángulo con respecto a un punto base
-bool compare(Point a, Point b, Point base) {
-    // Si los puntos están en la misma línea, devuelve el más lejano
-    int dx1 = a.x - base.x, dy1 = a.y - base.y;
-    int dx2 = b.x - base.x, dy2 = b.y - base.y;
-    if (dx1 * dy2 == dx2 * dy1) {
-        return (dx1 * dx1 + dy1 * dy1) < (dx2 * dx2 + dy2 * dy2);
-    }
-
-    // Devuelve el punto con un ángulo más bajo con respecto a la horizontal
-    int angle = (dx1 * dx2 + dy1 * dy2) / (sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2)));
-    int k = dy1 * dx2 - dx1 * dy2;
-    return k > 0 || (k == 0 && angle < 0);
-}
-
-// Función para encontrar el borde convexo de un conjunto de puntos
-vector<Point> convexHull(vector<Point>& points) {
+// Método para encontrar el borde convexo de un conjunto de puntos
+std::vector<Point> ConvexHull::find(const std::vector<Point>& points) {
     if (points.size() <= 1) return points;
 
     // Encontrar el punto más bajo y hacerlo el punto base
@@ -39,3 +18,11 @@ vector<Point> convexHull(vector<Point>& points) {
             minIndex = i;
         }
     }
+    std::vector<Point> sortedPoints(points);
+    std::swap(sortedPoints[0], sortedPoints[minIndex]);
+    Point base = sortedPoints[0];
+
+    // Ordenar los puntos por su ángulo con respecto a la base
+    std::sort(sortedPoints.begin() + 1, sortedPoints.end(), [base](Point a, Point b) {
+        return compare(a, b, base);
+    });
--- feature_ext_sel/shape/geometry/libreriaGeometria.h
+++ feature_ext_sel/shape/geometry/libreriaGeometria.h
@@ -1,12 +1,29 @@
 #ifndef LIBRERIAGEOMETRIA_H_INCLUDED
 #define LIBRERIAGEOMETRIA_H_INCLUDED
 
-struct Point;
+#pragma once
 
-bool compare(Point a, Point b, Point base);
-vector<Point> convexHull(vector<Point>& points); 
+#include <vector>
 
+// Estructura para almacenar un punto en 2D
+struct Point {
+    int x, y;
+    Point(int x = 0, int y = 0) : x(x), y(y) {}
+};
 
+// Clase ConvexHull para encontrar el borde convexo de un conjunto de puntos
+class ConvexHull {
+public:
+    // Constructor
+    ConvexHull();
+
+    // Método para encontrar el borde convexo de un conjunto de puntos
+    std::vector<Point> find(const std::vector<Point>& points);
+
+private:
+    // Función para comparar dos puntos por su ángulo con respecto a un punto base
+    static bool compare(Point a, Point b, Point base);
+};
 
 #endif // LIBRERIAGEOMETRIA_H_INCLUDED
 
