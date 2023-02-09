712dd1cd75cd70c6fc5bb0cd8001bd4b9b64055c
Bryan Gualoto <CLUBRELAMPAGOJR13@GMAIL.COM>
2023-02-09 07:03:40 -0500

    Geometry
    Geometry

--- feature_ext_sel/shape/geometry/libreriaGeometria.cpp
+++ feature_ext_sel/shape/geometry/libreriaGeometria.cpp
@@ -11,11 +11,3 @@ double media(double arr[], int n) {
     }
     return sum / n;
 }
-
-double desviacion_estandar(double arr[], int n) {
-    double mean = media(arr, n);
-    double sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += pow
-
-
