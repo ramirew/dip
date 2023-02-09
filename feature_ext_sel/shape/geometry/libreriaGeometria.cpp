28c2eb25664cb39dfa3633d600ea6cb76d571806
Bryan Gualoto <CLUBRELAMPAGOJR13@GMAIL.COM>
2023-02-09 06:01:52 -0500

    Grupo F - Geometry
    Grupo F - Geometry

--- feature_ext_sel/shape/geometry/libreriaGeometria.cpp
+++ feature_ext_sel/shape/geometry/libreriaGeometria.cpp
@@ -1,55 +1,115 @@
 #include "libreriaGeometria.h"
 
-void do_something(size_t size)
-{
-    // Declare an array of doubles to be allocated on the heap
-    double* numbers = new double[size]{ 0 };
+/* Hacer un algoritmo que:
+	a) Almacene números aleatorios (menores o iguales a 10) en una matriz de 5 * 6. 
+	b) Recorra la matriz: mostrando sus elementos
+	c) Imprima la suma de los números almacenados en la matriz. 
+	d) Imprima la suma de los números pares almacenados en la matriz
+	e) Imprima la suma de los números impares almacenados en la matriz
+	f) Imprima la suma de cada columnas
+	g) Imprima la suma de cada fila
+	h) Imprima la suma de los elementos de la diagonal principal
+	i) Imprima la transpuesta de la matriz
+	*/
 
-    // Assign a new value to the first element
-    numbers[0] = 1;
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-    // Assign a value to each subsequent element
-    // (numbers[1] is the second element in the array.)
-    for (size_t i = 1; i < size; i++)
-    {
-        numbers[i] = numbers[i - 1] * 1.1;
-    }
+#define FILAS 5
+#define COLUMNAS 6
+#define MAX_VALOR 10
 
-    // Access each element with subscript operator
-    for (size_t i = 0; i < size; i++)
-    {
-        std::cout << numbers[i] << " ";
-    }
+using namespace std;
 
-    // Access each element with pointer arithmetic
-    // Use a copy of the pointer for iterating
-    double* p = numbers;
 
-    for (size_t i = 0; i < size; i++)
-    {
-        // Dereference the pointer, then increment it
-        std::cout << *p++ << " ";
-    }
 
-    // Alternate method:
-    // Reset p to numbers[0]:
-    p = numbers;
-
-    // Use address of pointer to compute bounds.
-    // The compiler computes size as the number
-    // of elements * (bytes per element).
-    while (p < (numbers + size))
-    {
-        // Dereference the pointer, then increment it
-        std::cout << *p++ << " ";
-    }
-
-    delete[] numbers; // don't forget to do this!
-
+void llenarMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas) {
+	for (int i = 0; i < filas; ++i) {
+		for (int j = 0; j < columnas; ++j) {
+			matriz[i][j] = rand() % (MAX_VALOR + 1); // números aleatorios menores o iguales a MAX_VALOR
+		}
+	}
+}
+void mostrarMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas) {
+	cout << "La matriz es: " << endl;
+	for (int i = 0; i < filas; ++i) {
+		for (int j = 0; j < columnas; ++j) {
+			cout << "\t" << matriz[i][j];
+		}
+		cout << endl;
+	}
 }
 
+int sumarElementosMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas) {
+	int suma = 0;
+	for (int i = 0; i < filas; ++i) {
+		for (int j = 0; j < columnas; ++j) {
+			suma += matriz[i][j];
+		}
+	}
 
+	return suma;
+}
 
-   
+int sumarParesMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas) {
+	int suma = 0;
+	for (int i = 0; i < filas; ++i) {
+		for (int j = 0; j < columnas; ++j) {
+			if (matriz[i][j] % 2 == 0) {
+				suma += matriz[i][j];
+			}
+		}
+	}
+
+	return suma;
+}
+
+int sumarImparesMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas) {
+	int suma = 0;
+	for (int i = 0; i < filas; ++i) {
+		for (int j = 0; j < columnas; ++j) {
+			if (matriz[i][j] % 2 != 0) {
+				suma += matriz[i][j];
+			}
+		}
+	}
+
+	return suma;
+}
+
+void mostrarSumaColumnas(int matriz[FILAS][COLUMNAS], int filas, int columnas) {
+	int suma = 0;
+	cout << "\n";
+	for (int j = 0; j < columnas; ++j) {
+		suma = 0;
+		for (int i = 0; i < filas; ++i) {
+			suma += matriz[i][j];
+		}
+		cout << "La suma de la columna " << j << " es: " << suma << endl;
+	}
+}
+
+void mostrarSumaFilas(int matriz[FILAS][COLUMNAS], int filas, int columnas) {
+	int suma = 0;
+	cout << "\n";
+	for (int i = 0; i < filas; ++i) {
+		suma = 0;
+		for (int j = 0; j < columnas; ++j) {
+			suma += matriz[i][j];
+		}
+		cout << "La suma de la fila " << i << " es: " << suma << endl;
+	}
+}
+
+void mostrarMatrizTranspuesta(int matriz[FILAS][COLUMNAS], int filas, int columnas) {
+	cout << "\nLa matriz transpuesta es: " << endl;
+	for (int j = 0; j < columnas; ++j) {
+		for (int i = 0; i < filas; ++i) {
+			cout << "\t" << matriz[i][j];
+		}
+		cout << endl;
+	}
+}
 
 
--- feature_ext_sel/shape/geometry/libreriaGeometria.h
+++ feature_ext_sel/shape/geometry/libreriaGeometria.h
@@ -1,20 +1,14 @@
 #ifndef LIBRERIAGEOMETRIA_H_INCLUDED
 #define LIBRERIAGEOMETRIA_H_INCLUDED
 
-void asymmetry(Point points[], int n);
-void circulary(Point points[], int n);
-void compactness(Point points[], int n);
-void convex(Point points[], int n);
-void elongation(Point points[], int n);
-void form(Point points[], int n);
-void fourier(Point points[], int n);
-void distance(Point points[], int n);
-void radial(Point points[], int n);
-void radius(Point points[], int n);
-void gradient(Point points[], int n);
-void roundness(Point points[], int n);
-void solidity(Point points[], int n);
-void geometry(Point points[], int n);
+void llenarMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas);
+void mostrarMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas);
+int sumarElementosMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas);
+int sumarParesMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas);
+int sumarImparesMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas);
+void mostrarSumaColumnas(int matriz[FILAS][COLUMNAS], int filas, int columnas);
+void mostrarSumaFilas(int matriz[FILAS][COLUMNAS], int filas, int columnas);
+void mostrarMatrizTranspuesta(int matriz[FILAS][COLUMNAS], int filas, int columnas);
 #include  "libreriaGeometria.cpp"
 
 #endif // LIBRERIAGEOMETRIA_H_INCLUDED
