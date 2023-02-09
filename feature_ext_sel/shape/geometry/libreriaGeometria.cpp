8b801bdc59f95b040319bc704cf5eeda913e99c9
Bryan Gualoto <CLUBRELAMPAGOJR13@GMAIL.COM>
2023-02-09 05:50:56 -0500

    Grupo F - Geometry
    Grupo F - Geometry

--- feature_ext_sel/shape/geometry/libreriaGeometria.cpp
+++ feature_ext_sel/shape/geometry/libreriaGeometria.cpp
@@ -1,202 +1,51 @@
 #include "libreriaGeometria.h"
 
-#include <iostream>
-#include <stack>
-#include <stdlib.h>
-#include<bits/stdc++.h>
-using namespace std;
-
-struct Point
+void do_something(size_t size)
 {
-    int x, y;
-};
-// A global point needed for  sorting points with reference
-// to  the first point Used in compare function of qsort()
-Point p0;
+    // Declare an array of doubles to be allocated on the heap
+    double* numbers = new double[size]{ 0 };
 
-// A utility function to find next to top in a stack
-Point nextToTop(stack<Point> &S)
-{
-    Point p = S.top();
-    S.pop();
-    Point res = S.top();
-    S.push(p);
-    return res;
-}
+    // Assign a new value to the first element
+    numbers[0] = 1;
 
-// A utility function to swap two points
-void swap(Point &p1, Point &p2)
-{
-    Point temp = p1;
-    p1 = p2;
-    p2 = temp;
-}
-
-// A utility function to return square of distance
-// between p1 and p2
-int distSq(Point p1, Point p2)
-{
-    return (p1.x - p2.x)*(p1.x - p2.x) +
-          (p1.y - p2.y)*(p1.y - p2.y);
-}
-
-// To find orientation of ordered triplet (p, q, r).
-// The function returns following values
-// 0 --> p, q and r are collinear
-// 1 --> Clockwise
-// 2 --> Counterclockwise
-int orientation(Point p, Point q, Point r)
-{
-    int val = (q.y - p.y) * (r.x - q.x) -
-              (q.x - p.x) * (r.y - q.y);
-
-    if (val == 0) return 0;  // collinear
-    return (val > 0)? 1: 2; // clock or counterclock wise
-}
-
-// A function used by library function qsort() to sort an array of
-// points with respect to the first point
-int compare(const void *vp1, const void *vp2)
-{
-   Point *p1 = (Point *)vp1;
-   Point *p2 = (Point *)vp2;
-
-   // Find orientation
-   int o = orientation(p0, *p1, *p2);
-   if (o == 0)
-     return (distSq(p0, *p2) >= distSq(p0, *p1))? -1 : 1;
-
-   return (o == 2)? -1: 1;
-}
-
-
-
-// A function used by library function qsort() to sort an array of
-// points with respect to the first point
-int compare(const void *vp1, const void *vp2)
-{
-   Point *p1 = (Point *)vp1;
-   Point *p2 = (Point *)vp2;
-
-   // Find orientation
-   int o = orientation(p0, *p1, *p2);
-   if (o == 0)
-     return (distSq(p0, *p2) >= distSq(p0, *p1))? -1 : 1;
-
-   return (o == 2)? -1: 1;
-}
-
-void compactness(Point points[], int n) //Función para calcular el casco convexo
-{
-    if (n < 3) return;  //Determina que debe tener al menos 3 puntos
-    vector<Point> hull;  //Inicializa el resultado
-  
-   //Encuentra el punto que este más a la izquiera
-    int l = 0;
-    for (int i = 1; i < n; i++)
-        if (points[i].x < points[l].x)
-            l = i;
- 
-   // Hace que el punto que estaba más a la izquierda se mueva en sentido horario
-    int p = l, q;
-    do
+    // Assign a value to each subsequent element
+    // (numbers[1] is the second element in the array.)
+    for (size_t i = 1; i < size; i++)
     {
-        hull.push_back(points[p]);  //Agrega un punto actual al resultado
- 
-       //Busca un punto para determinar en que sentido se encuentra la imagen
-        q = (p+1)%n;
-        for (int i = 0; i < n; i++)
-        {
-          // Si el punto se encuentra en sentido antihorario se actualiza el punto
-           if (orientation(points[p], points[i], points[q]) == 2)
-               q = i;
-        }
- 
- 
-        p = q;
- 
-    } while (p != l);  //Mientras no llegue al primer punto
- 
-    // Imprime el resultado obtenido
-    for (int i = 0; i < hull.size(); i++)
-        cout << "(" << hull[i].x << ", "
-              << hull[i].y << ")\n";
+        numbers[i] = numbers[i - 1] * 1.1;
+    }
+
+    // Access each element with subscript operator
+    for (size_t i = 0; i < size; i++)
+    {
+        std::cout << numbers[i] << " ";
+    }
+
+    // Access each element with pointer arithmetic
+    // Use a copy of the pointer for iterating
+    double* p = numbers;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        // Dereference the pointer, then increment it
+        std::cout << *p++ << " ";
+    }
+
+    // Alternate method:
+    // Reset p to numbers[0]:
+    p = numbers;
+
+    // Use address of pointer to compute bounds.
+    // The compiler computes size as the number
+    // of elements * (bytes per element).
+    while (p < (numbers + size))
+    {
+        // Dereference the pointer, then increment it
+        std::cout << *p++ << " ";
+    }
+
+    delete[] numbers; // don't forget to do this!
+
 }
-// Prints convex hull of a set of n points.
-void geometry(Point points[], int n)
-{
-   // Find the bottommost point
-   int ymin = points[0].y, min = 0;
-   for (int i = 1; i < n; i++)
-   {
-     int y = points[i].y;
-
-     // Pick the bottom-most or choose the left
-     // most point in case of tie
-     if ((y < ymin) || (ymin == y &&
-         points[i].x < points[min].x))
-        ymin = points[i].y, min = i;
-   }
-
-   // Place the bottom-most point at first position
-   swap(points[0], points[min]);
-
-   // Sort n-1 points with respect to the first point.
-   // A point p1 comes before p2 in sorted output if p2
-   // has larger polar angle (in counterclockwise
-   // direction) than p1
-   p0 = points[0];
-   qsort(&points[1], n-1, sizeof(Point), compare);
-
-   // If two or more points make same angle with p0,
-   // Remove all but the one that is farthest from p0
-   // Remember that, in above sorting, our criteria was
-   // to keep the farthest point at the end when more than
-   // one points have same angle.
-   int m = 1; // Initialize size of modified array
-   for (int i=1; i<n; i++)
-   {
-       // Keep removing i while angle of i and i+1 is same
-       // with respect to p0
-       while (i < n-1 && orientation(p0, points[i],
-                                    points[i+1]) == 0)
-          i++;
-
-
-       points[m] = points[i];
-       m++;  // Update size of modified array
-   }
-
-   // If modified array of points has less than 3 points,
-   // convex hull is not possible
-   if (m < 3) return;
-
-   // Create an empty stack and push first three points
-   // to it.
-   stack<Point> S;
-   S.push(points[0]);
-   S.push(points[1]);
-   S.push(points[2]);
-
-   // Process remaining n-3 points
-   for (int i = 3; i < m; i++)
-   {
-      // Keep removing top while the angle formed by
-      // points next-to-top, top, and points[i] makes
-      // a non-left turn
-      while (S.size()>1 && orientation(nextToTop(S), S.top(), points[i]) != 2)
-         S.pop();
-      S.push(points[i]);
-   }
-
-   // Now stack has the output points, print contents of stack
-   while (!S.empty())
-   {
-       Point p = S.top();
-       cout << "(" << p.x << ", " << p.y <<")" << endl;
-       S.pop();
-   }
-}
-
 
 
