e81f434d32315a810b4235baa84a425afddaf15a
Bryan Gualoto <CLUBRELAMPAGOJR13@GMAIL.COM>
2023-02-09 06:03:57 -0500

    Grupo F - Geometry
    Grupo F - Geometry

--- feature_ext_sel/shape/geometry/libreriaGeometria.h
+++ feature_ext_sel/shape/geometry/libreriaGeometria.h
@@ -1,14 +1,6 @@
 #ifndef LIBRERIAGEOMETRIA_H_INCLUDED
 #define LIBRERIAGEOMETRIA_H_INCLUDED
 
-void llenarMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas);
-void mostrarMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas);
-int sumarElementosMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas);
-int sumarParesMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas);
-int sumarImparesMatriz(int matriz[FILAS][COLUMNAS], int filas, int columnas);
-void mostrarSumaColumnas(int matriz[FILAS][COLUMNAS], int filas, int columnas);
-void mostrarSumaFilas(int matriz[FILAS][COLUMNAS], int filas, int columnas);
-void mostrarMatrizTranspuesta(int matriz[FILAS][COLUMNAS], int filas, int columnas);
 #include  "libreriaGeometria.cpp"
 
 #endif // LIBRERIAGEOMETRIA_H_INCLUDED
