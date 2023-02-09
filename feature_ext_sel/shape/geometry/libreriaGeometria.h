1bde07bb9134d4dc16c3fb297958a98a5643f18a
Bryan Gualoto <CLUBRELAMPAGOJR13@GMAIL.COM>
2023-02-09 06:36:35 -0500

    Grupo F - Geometry
    Grupo F - Geometry

--- feature_ext_sel/shape/geometry/libreriaGeometria.h
+++ feature_ext_sel/shape/geometry/libreriaGeometria.h
@@ -1,16 +1,10 @@
 #ifndef LIBRERIAGEOMETRIA_H_INCLUDED
 #define LIBRERIAGEOMETRIA_H_INCLUDED
 
-int aleatorio_en_rango(int minimo, int maximo);
-void llenarMatriz(float matriz);
-void imprimirLinea();
-void imprimir(float matriz, char alumnos);
-void imprimir(float matriz,char alumnos);
-float mean(float arr[], int n);
-float standardDeviation(float arr[],int n);
-float skewness(float arr[], int n);
-unsigned int bitReverse(unsigned int x, int log2n);
-void fft(Iter_T a, Iter_T b, int log2n,double PI);
+
+float resolverEcuacion(float valor)
+float resolverDerivada(float valor) 
+
 #include  "libreriaGeometria.cpp"
 
 #endif // LIBRERIAGEOMETRIA_H_INCLUDED
