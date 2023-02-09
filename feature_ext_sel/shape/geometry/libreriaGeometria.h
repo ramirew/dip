6528ff72de72b9e4fa2136722a0a91fc44ce5824
Bryan Gualoto <CLUBRELAMPAGOJR13@GMAIL.COM>
2023-02-09 07:00:58 -0500

    Geometry
    Geometry

--- feature_ext_sel/shape/geometry/libreriaGeometria.h
+++ feature_ext_sel/shape/geometry/libreriaGeometria.h
@@ -1,11 +1,8 @@
 #ifndef LIBRERIAGEOMETRIA_H_INCLUDED
 #define LIBRERIAGEOMETRIA_H_INCLUDED
 
-int aleatorio_en_rango(int minimo, int maximo);
-void llenarMatriz(float matriz);
-void imprimirLinea();
-void imprimir(float matriz, char alumnos);
-void imprimir(float matriz,char alumnos);
+double media(double arr[], int n);
+
 #include  "libreriaGeometria.cpp"
 
 #endif // LIBRERIAGEOMETRIA_H_INCLUDED
