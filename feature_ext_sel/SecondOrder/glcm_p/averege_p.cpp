8f178009766182ac842ea1273b4def823f3ec9b1
diegoxBV <85522635+diegoxBV@users.noreply.github.com>
2023-02-14 23:06:10 -0500

    Update averege_p.cpp

--- feature_ext_sel/SecondOrder/glcm_p/averege_p.cpp
+++ feature_ext_sel/SecondOrder/glcm_p/averege_p.cpp
@@ -24,15 +24,26 @@ averege_p::averege_p(){
 }
 
 //Initialize functions that have been used for measuring co-occurence matrixes for 0,45,90,135 degree angle
+/*CoOcMat_Angle_0: calcula la matriz de co-ocurrencia para un ángulo de 0 grados.
+CoOcMat_Angle_45: calcula la matriz de co-ocurrencia para un ángulo de 45 grados.
+CoOcMat_Angle_90: calcula la matriz de co-ocurrencia para un ángulo de 90 grados.
+CoOcMat_Angle_135: calcula la matriz de co-ocurrencia para un ángulo de 135 grados.
+*/
 double** CoOcMat_Angle_0   (int distance, u_int16_t **grays, int rows, int cols, int* tone_LUT, int tone_count);
 double** CoOcMat_Angle_45  (int distance, u_int16_t **grays, int rows, int cols, int* tone_LUT, int tone_count);
 double** CoOcMat_Angle_90  (int distance, u_int16_t **grays, int rows, int cols, int* tone_LUT, int tone_count);
 double** CoOcMat_Angle_135 (int distance, u_int16_t **grays, int rows, int cols, int* tone_LUT, int tone_count);
 
 //INICIALIZAR FUNCIONES
+//f1_asm: calcula el momento angular de la energía.
 double f1_asm (double **P, int Ng);
 
 //RECURSOS DE LOS METODOS
+/**funciones de utilidad para la asignación de memoria:
+    allocate_vector: asigna memoria para un vector.
+    allocate_matrix: asigna memoria para una matriz.
+    free_matrix: libera memoria asignada para una matriz.
+*/
 double *allocate_vector (int nl, int nh);
 double **allocate_matrix (int nrl, int nrh, int ncl, int nch);
 void free_matrix(double **matrix,int nrh);
@@ -40,6 +51,9 @@ void free_matrix(double **matrix,int nrh);
 
 
 //4 SUM AVEREGE
+/**
+El método f6_savg recibe una matriz de co-ocurrencia P, el número de niveles de gris Ng y el número de hilos hilos. Este método calcula una medida estadística llamada promedio de la suma (savg) utilizando la matriz de co-ocurrencia.
+*/
 double averege_p::f6_savg (double **P, int Ng, int hilos) const {
     int i, j;
     double savg = 0;
