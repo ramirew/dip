eaa7c42d350eb785092a71b1c84055c65ee33df2
diegoxBV <85522635+diegoxBV@users.noreply.github.com>
2023-02-14 23:54:47 -0500

    Update secondangular_p.h

--- feature_ext_sel/SecondOrder/glcm_p/secondangular_p.h
+++ feature_ext_sel/SecondOrder/glcm_p/secondangular_p.h
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <vector>
 
+/*
+//se define una clase llamada "secondangular" que tiene un constructor público y una función pública llamada "f1_asm" que toma una matriz de doble puntero "P" 
+y un entero "Ng" como argumentos y devuelve un valor de tipo double.
+*/
+
 using namespace std;
 
 class secondangular_p;
