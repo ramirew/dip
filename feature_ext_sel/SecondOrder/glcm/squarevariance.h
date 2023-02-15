312ac0946388e74dfc09c7e9788bb368e248593c
diegoxBV <85522635+diegoxBV@users.noreply.github.com>
2023-02-14 23:33:58 -0500

    Update squarevariance.h

--- feature_ext_sel/SecondOrder/glcm/squarevariance.h
+++ feature_ext_sel/SecondOrder/glcm/squarevariance.h
@@ -4,6 +4,10 @@
 #include <iostream>
 #include <vector>
 
+/*
+se define una clase "squarevariance" con un constructor y una función pública, pero no incluye su implementación. La implementación de la función "f4_var" 
+se encuentra en el .cpp
+*/
 using namespace std;
 
 class squarevariance;
