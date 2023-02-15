2938cd2e3eec4c374aa017209879ce02333a8ebd
diegoxBV <85522635+diegoxBV@users.noreply.github.com>
2023-02-14 23:51:49 -0500

    Update moment_p.h

--- feature_ext_sel/SecondOrder/glcm_p/moment_p.h
+++ feature_ext_sel/SecondOrder/glcm_p/moment_p.h
@@ -4,6 +4,13 @@
 #include <iostream>
 #include <vector>
 
+/*
+se define una clase llamada "moment". La clase tiene un constructor predeterminado y una función llamada "f5_idm" que toma una matriz 2D "P" 
+y un entero "Ng" como argumentos y devuelve un valor de punto flotante "double".
+
+La función "f5_idm" calcula un momento de segundo orden denominado "Inversión Diferencial de Momento"
+*/
+
 using namespace std;
 
 class moment_p;
