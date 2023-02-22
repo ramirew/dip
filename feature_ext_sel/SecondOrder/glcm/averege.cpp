bdfd1030f4530a4ada6d3ccc8de1e61b12b3ffd9
diegoxBV <85522635+diegoxBV@users.noreply.github.com>
2023-02-22 10:35:30 -0500

    Update averege.cpp

--- feature_ext_sel/SecondOrder/glcm/averege.cpp
+++ feature_ext_sel/SecondOrder/glcm/averege.cpp
@@ -6,8 +6,20 @@ using namespace std;
 averege::averege(){}
 
 //RECURSOS DE LOS METODOS
-double *allocate_vector (int nl, int nh);
 
+/*
+*/
+double *allocate_vector (int nl, int nh);
+/*
+ "allocate_vector" que reserva memoria dinámicamente para un vector Pxpy de longitud 2*Ng y 
+lo inicializa con ceros. Luego, la función "f6_savg" recorre todos los elementos de la matriz P, los suma en el vector Pxpy y finalmente calcula la 
+suma ponderada de los elementos de Pxpy.
+*/
+
+/*
+La función devuelve el resultado de la suma ponderada. Es importante liberar la memoria dinámicamente
+asignada para el vector Pxpy utilizando la función "free" al final de la función "f6_savg".
+*/
 //4 SUM AVEREGE
 double averege::f6_savg (double **P, int Ng) const {
     int i, j;
